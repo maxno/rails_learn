b39f8640fe7c9e636056b843d420165d48d30228
maxno <olshanskyi@gmail.com>
2011-12-21 00:39:53 +0200

    My app.

---
+++ my_learn/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyLearn::Application.config.secret_token = 'b2d480a44567e5afd3adc1ce12af7a5292329187d3f4fb803f0797276217a1f217ea02b96017c3d4c899a6d41fa3fee314431c5f86800323446fb92dfd562afa'
